1651293b542acc6a2418bddeb00719fb6f0d1614
magriffsla <soho51@mail.ru>
2016-02-24 11:08:51 +0300

    susleg001

---
+++ src/kz1/Models/RegaVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kz1.Models
+{
+    public class RegaVM
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confirm { get; set; }
+        public bool remember { get; set; }
+    }
+}
